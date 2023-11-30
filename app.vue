86f461b28cf94c401dd3f304f1db025fc14c4b6f
Paul Guilbert <paul.guilbert@backmarket.com>
2023-11-30 12:42:51 +0100

    add default demo

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/await-default">await-default</NuxtLink></li>
+      <li><NuxtLink to="/noawait-default">noawait-default</NuxtLink></li>
+    </ul>
   </div>
   <NuxtPage />
 </template>
---
+++ pages/await-default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const asyncData = await useAsyncData(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve("data"), 4000);
+    })
+);
+
+const asyncData1 = await useAsyncData(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve("data"), 4000);
+    })
+);
+</script>
+
+<template>
+  <div>
+    {{ asyncData.data ?? "Loading..." }}
+    {{ asyncData1.data ?? "Loading..." }}
+  </div>
+</template>
